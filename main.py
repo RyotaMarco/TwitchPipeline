dbbd7ad01fb98c4b97d9bb45d3bc602fb7fdd521
MarcoRyota <marcos@nouva.se>
2025-04-16 01:08:30 -0300

    feat: get_streams changed
    get_streams changed to make GET requests on entire API pages

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from src.api.twitch_api import *
 
-print(get_filtered_params())
+'''print(get_streams())
 
+print(get_filtered_params())'''
+
+data_api = get_streams()
+
+print(get_filtered_params(data_api))
--- src/api/twitch_api.py
+++ src/api/twitch_api.py
@@ -1,3 +1,4 @@
+import time
 from config.twitch_config import get_headers
 import requests
 
@@ -8,19 +9,59 @@ Access the Twitch API with the correct parameters for this project
 
 BASE_URL = 'https://api.twitch.tv/helix'
 
-def get_streams(user_login=None,  game_id=None):
-    url = f"{BASE_URL}/streams"
+def get_streams():
+    
+    url = f"{BASE_URL}/streams?"
+    cursor = None
 
-    params = {
-        'type': 'live',
-        'first': '100'
-    }
+
+
 
     headers = get_headers()
 
-    response = requests.get(url, headers=headers, params=params)
-    response.raise_for_status()
-    return response.json()
+    results_api = []
+
+    while True:
+
+        params = {
+            'type': 'live',
+            'first': '100',
+        }
+
+        if cursor:
+            params['after'] = cursor
+        response = requests.get(url, headers=headers, params=params)
+        response.raise_for_status()
+        response_json = response.json()
+        cursor = response_json.get('pagination', {}).get('cursor')
+        results_api.extend(response_json['data'])
+        if not cursor:
+            break
+        
+        time.sleep(0.5)
+    return results_api
+
+
+
+def get_filtered_params(data):
+
+
+    filtered_json= {
+    'data_fill': [
+            {
+                'user_id':filtered_data['user_id'],
+                'user_name':filtered_data['user_name'],
+                'Stream_id':filtered_data['id'],
+                'viewer_count':filtered_data['viewer_count'],
+                'game_name':filtered_data['game_name'],
+                'started_at':filtered_data['started_at'],
+                'is_mature':filtered_data['is_mature'],
+                'thumbnail_url':filtered_data['thumbnail_url']
+            }
+            for filtered_data in data
+        ]
+    }    
+    return filtered_json
 
 def get_viewers_by_category():
     twitch_request = get_streams()
@@ -37,24 +78,4 @@ def get_viewers_by_category():
             category_viewers[category] = viewers
 
     return category_viewers
-
-def get_filtered_params():
-    twitch_request = get_streams()
-
-    filtered_json= {
-    'data_fill': [
-            {
-                'user_id':data['user_id'],
-                'user_name':data['user_name'],
-                'Stream_id':data['id'],
-                'viewer_count':data['viewer_count'],
-                'game_name':data['game_name'],
-                'started_at':data['started_at'],
-                'is_mature':data['is_mature'],
-                'thumbnail_url':data['thumbnail_url']
-            }
-            for data in twitch_request['data']
-        ]
-    }    
-    return filtered_json
     
